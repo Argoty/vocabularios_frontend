f08dd37c403f7023c5ad3db8b537072690baac2b
JaxisArty <jaxis0810@gmail.com>
2023-02-24 19:48:47 -0500

    ahora si con variables de entorno

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios';
 
-
 axios.defaults.baseURL = "https://vocabularios-1-x3183952.deta.app/"
 
 Vue.config.productionTip = false
